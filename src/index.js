d19ff81ef28d6abc57a1956590fc9e6378aa2621
Mastersandy007 <mercedesintwoyears@gmail.com>
2022-08-11 09:36:50 -0700

    "modified index.js"

--- src/index.js
+++ src/index.js
@@ -8,70 +8,8 @@ const App = () => {
     return (
         <div className="ui container comments">
             <CommentDetails author="Sandy" timeAgo="Today at 10:00PM" content="Nice blog post!"/>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-            
-                <div className="content">
-                    <a href="/" className="author">
-                        sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-            
-                <div className="content">
-                    <a href="/" className="author">
-                        Sony
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"  src={ faker.image.avatar() } />
-                </a>
-            
-                <div className="content">
-                    <a href="/" className="author">
-                        ram
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 7:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post! 2</div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={ faker.image.avatar() } />
-                </a>
-            
-                <div className="content">
-                    <a href="/" className="author">
-                        bam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 8:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post! 3</div>
-                </div>
-            </div>
-
+            <CommentDetails author="Sam" timeAgo="Today at 5:00PM" content="Amazing !"/>
+            <CommentDetails author="Ram" timeAgo="Today at 2:00PM" content="Good Idea !"/>
         </div>
     );
 
